7575cc5b0cf8efc2aae3feef606d9b091960b4d6
Girma Nigusse <xgirma@users.noreply.github.com>
2020-02-13 20:10:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Playwright: (CommonJS, Jest and Expect)
+
+[![GitHub Actions status | e2e-boilerplates/playwright-commonjs-jest-expect](https://github.com/e2e-boilerplates/playwright-commonjs-jest-expect/workflows/playwright-commonjs-jest-expect/badge.svg)](https://github.com/e2e-boilerplates/playwright-commonjs-jest-expect/actions?workflow=playwright-commonjs-jest-expect)
+
+Playwright end-to-end test automation boilerplate, using CommonJS, Jest and Expect.
+
+## Getting Started
+
+    git clone git@github.com:e2e-boilerplates/playwright-commonjs-jest-expect.git
+    cd playwright-commonjs-jest-expect
+    npm install
+    npm run test
---
+++ __tests__/sandbox.spec.js
@@ -0,0 +1,34 @@
+const { chromium } = require("playwright");
+
+let page;
+let browser;
+
+describe("Sandbox", () => {
+  beforeAll(async () => {
+    browser = process.env.GITHUB_ACTIONS
+      ? await chromium.launch()
+      : await chromium.launch({ headless: false });
+    const context = await browser.newContext();
+    page = await context.newPage();
+
+    await page
+      .goto("https://e2e-boilerplates.github.io/sandbox/", {
+        waitUntil: "networkidle0"
+      })
+      .catch(() => {});
+  });
+
+  afterAll(() => {
+    if (!page.isClosed()) {
+      browser.close();
+    }
+  });
+
+  test("should be on the sandbox", async () => {
+    await page.waitFor("h1");
+    const title = await page.$eval("h1", el => el.textContent);
+
+    expect(await page.title()).toEqual("Sandbox");
+    expect(title).toEqual("Sandbox");
+  });
+});
